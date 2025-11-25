97ca9fa5e4da25fc7451f8541c33524e4a86aa0e
Noah <noah.westwind@gmail.com>
2025-11-24 23:38:49 -0800

    this file agian!

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-<<<<<<< HEAD
-import "./.next/dev/types/routes.d.ts";
-=======
 import "./.next/types/routes.d.ts";
->>>>>>> 3115731ec (out-prod)
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
